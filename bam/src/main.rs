ef7c197e60b6bc7d952c7a1774e9cce165e479fc
Mahmoud Mazouz <mazouz.mahmoud@outlook.com>
2022-08-10 00:35:30 +0200

    Restore streaming mode in REPL

--- bam/src/main.rs
+++ bam/src/main.rs
@@ -5,7 +5,7 @@ use anyhow::{anyhow, bail, Context, Error, Result};
 use chumsky::Parser;
 use clap;
 
-use compiler::Compiler;
+use compiler::{Compiler, Handle};
 use rustyline::{error::ReadlineError, Editor};
 use tracing::Level;
 
@@ -17,11 +17,14 @@ mod syntax;
 mod types;
 mod util;
 
-use eval::Loop;
+use eval::{Loop, SharedFlow};
 pub use lexer::{LexerBuilder, Token, KEYWORD_MAP};
 use types::GlobalTypeEnv;
 
-use crate::eval::Factory;
+use crate::{
+    compiler::Instr,
+    eval::{Factory, SharedEnv},
+};
 // use crate::{compiler::Compiler, eval::Factory};
 pub use crate::{
     parser::ParserBuilder,
@@ -67,9 +70,7 @@ fn main() -> Result<()> {
     let args: Args = clap::Parser::parse();
 
     if args.trace {
-        tracing_subscriber::fmt()
-            .with_max_level(Level::TRACE)
-            .init();
+        tracing_subscriber::fmt().init();
     }
 
     run_repl(args.filename)
@@ -85,7 +86,7 @@ enum Mode {
     /// Stores the current line number.
     Definiton(usize),
     /// If in this mode, we're stepping through a stream.
-    Streaming(Loop),
+    Streaming(Handle, SharedEnv),
 }
 
 fn run_repl(filename: Option<String>) -> Result<()> {
@@ -149,7 +150,7 @@ fn run_repl(filename: Option<String>) -> Result<()> {
             .collect()
     };
 
-    let _parse_statement = |source: String| -> Result<Stream> {
+    let parse_stream = |source: String| -> Result<Stream> {
         let tokens = lexer.parse(source).map_err(|errs| {
             anyhow!(
                 "{}",
@@ -179,6 +180,7 @@ fn run_repl(filename: Option<String>) -> Result<()> {
         Ok(machines) => {
             program.machines.extend(machines.into_iter());
             let module = Compiler::new().transform(program.clone());
+            println!("{module}");
             *factory = Factory::new(module);
         }
         Err(err) => report(err),
@@ -186,12 +188,43 @@ fn run_repl(filename: Option<String>) -> Result<()> {
 
     let prompt = |mode: &mut Mode| match mode {
         Mode::Statement => Colour::Blue.bold().paint("bam> ").to_string(),
-        Mode::Streaming(_) => Colour::Purple.bold().paint("BAM!> ").to_string(),
+        Mode::Streaming(_, _) => Colour::Purple.bold().paint("BAM!> ").to_string(),
         Mode::Definiton(l) => {
             *l += 1;
             Colour::White.bold().paint(format!("{l} | ")).to_string()
         }
     };
+
+    let mut load_stream =
+        |source: String, mode: &mut Mode, factory: Factory| match parse_stream(source) {
+            Ok(stream) => {
+                let dummy = Definition {
+                    name: String::from("Unkown"),
+                    body: Vec::new(),
+                    result: stream,
+                };
+                let mut compiler = Compiler::new();
+                compiler.transform_machine(dummy);
+
+                let env = SharedEnv::new();
+                for instr in compiler.current().clone().unpack() {
+                    match instr {
+                        Instr::Make(handle, fstream) => {
+                            let sflow = factory.clone().compile(fstream);
+                            env.set(handle, sflow);
+                        }
+                        Instr::Next(_) => {
+                            eprintln!("Soemthing bad happened");
+                        }
+                        Instr::Exit(handle) => {
+                            *mode = Mode::Streaming(handle, env);
+                            return;
+                        }
+                    }
+                }
+            }
+            Err(err) => report(err),
+        };
 
     // Entry point.
 
@@ -205,9 +238,11 @@ fn run_repl(filename: Option<String>) -> Result<()> {
     loop {
         match editor.readline(&prompt(&mut mode)) {
             Ok(line) if line.trim().is_empty() => {
-                if let Mode::Streaming(ref mut values) = mode {
-                    match values.step() {
-                        Ok(value) => println!("{}", value),
+                if let Mode::Streaming(hdl, ref env) = mode {
+                    match env.get(hdl).unwrap().next(&env) {
+                        Ok(value) => {
+                            println!("{value}");
+                        }
                         Err(err) => report(err),
                     };
                 }
@@ -229,10 +264,11 @@ fn run_repl(filename: Option<String>) -> Result<()> {
             }
             Ok(line) if matches!(mode, Mode::Statement) => {
                 editor.add_history_entry(line.as_str());
-                let values = factory.make_loop(&line);
-                mode = Mode::Streaming(values);
+                load_stream(line, &mut mode, factory.clone());
             }
-            Err(ReadlineError::Eof) if matches!(mode, Mode::Streaming(_)) => mode = Mode::Statement,
+            Err(ReadlineError::Eof) if matches!(mode, Mode::Streaming(_, _)) => {
+                mode = Mode::Statement
+            }
             Err(ReadlineError::Eof) if matches!(mode, Mode::Definiton(_)) => {
                 let lines = definition_buf.drain(..).collect::<String>();
                 editor.add_history_entry(lines.as_str());
